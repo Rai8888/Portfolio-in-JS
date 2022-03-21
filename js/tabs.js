1085bab5469894062ff84a9f781966b640197df9
Raiska8 <87840244+Raiska8@users.noreply.github.com>
2022-03-22 02:04:36 +0300

    fix: updated

--- js/tabs.js
+++ js/tabs.js
@@ -1,46 +1,44 @@
-const tabButtons = document.querySelectorAll('.design-list__item')
-const tabDescriptions = document.querySelectorAll('.design__descr')
-const tabImages = document.querySelectorAll('.design-images')
-const tabSectionImages = document.querySelectorAll('.feature__img')
+const titleDocument = document.querySelector("title");
+const tabButtons = document.querySelectorAll(".design-list__item");
+const tabDescriptions = document.querySelectorAll(".design__descr");
+const tabImages = document.querySelectorAll(".design-images");
+const tabSecImage = document.querySelectorAll(".design-block__img");
+const tabTitle = document.querySelectorAll(".design__title");
 
 const changeContent = (array, value) => {
   array.forEach((elem) => {
     if (elem.dataset.tabsField === value) {
-      elem.classList.remove('hidden')
+      elem.classList.remove("hidden");
     } else {
-      elem.classList.add('hidden')
+      elem.classList.add("hidden");
     }
-  })
-}
+  });
+};
+
+const changeTitle = (array, value) => {
+  array.forEach((elem) => {
+    if (elem.dataset.tabsField === value) {
+      titleDocument.textContent = elem.textContent;
+    }
+  });
+};
 
 tabButtons.forEach((tabButton) => {
-  tabButton.addEventListener('click', (event) => {
-const dataValue = tabButton.dataset.tabsHandler 
-changeContent(tabDescriptions, dataValue)
-changeContent(tabImages, dataValue)
-changeContent(tabSectionImages, dataValue)
+  tabButton.addEventListener("click", (event) => {
+    const dataValue = tabButton.dataset.tabsHandler;
 
-tabButtons.forEach((btn) => {
-  if (btn === event.target) {
-    btn.classList.add('design-list__item_active')
-  } else {
-    btn.classList.remove('design-list__item_active')
-  }
-})
-  })
-})
+    changeContent(tabDescriptions, dataValue);
+    changeContent(tabImages, dataValue);
+    changeContent(tabSecImage, dataValue);
+    changeContent(tabTitle, dataValue);
+    changeTitle(tabTitle, dataValue);
 
-/*-- Менять title вкладки браузера на текст из заголовка над табами --*/
-the_title = document.querySelector('title');
-
-document.querySelector('#the_button').onclick = function () { 
- the_title.innerHTML = "Примеры сайтов"; 
-}
-
-the_title = document.querySelector('title');
-
-document.querySelector('#the_button2').onclick = function () { 
-  the_title.innerHTML = "Примеры приложений"; 
-}
-
-  
+    tabButtons.forEach((btn) => {
+      if (btn === event.target) {
+        btn.classList.add("design-list__item_active");
+      } else {
+        btn.classList.remove("design-list__item_active");
+      }
+    });
+  });
+});
